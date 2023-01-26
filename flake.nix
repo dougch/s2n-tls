a868ab23a6ee32b7460ffb34c8bd51405c81d1b9
Doug Chapman <dougch@amazon.com>
2023-01-26 21:39:14 +0000

    Working libcrypto overrides

--- flake.nix
+++ flake.nix
@@ -13,16 +13,30 @@
         inherit system; 
 
         buildInputs = [ pkgs.cmake
-                        pkgs.openssl ]; # s2n-config has find_dependency(LibCrypto
+                        pkgs.ninja
+                        pkgs.openssl_3 ]; # s2n-config has find_dependency LibCrypto
 
         cmakeFlags = [
             "-DBUILD_SHARED_LIBS=ON"
             "-DUNSAFE_TREAT_WARNINGS_AS_ERRORS=OFF" # disable -Werror
             "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
-            "-DS2N_NO_PQ=1"
+            "-DS2N_NO_PQ=1" # TODO: set when system like aarch64
         ];
 
       };
+      packages.s2n-tls-openssl11 = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+        buildInputs = [ pkgs.openssl_1_1 ]; # s2n-config has find_dependency LibCrypto
+      });
+      packages.s2n-tls-libressl = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+        buildInputs = [ pkgs.libressl ]; # s2n-config has find_dependency LibCrypto
+      });
+      # TODO: boringssl shared lib no being found by cmake
+      packages.s2n-tls-boringssl = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+        buildInputs = [ pkgs.boringssl ]; # s2n-config has find_dependency LibCrypto
+      });
       defaultPackage = packages.s2n-tls;
       
    });
