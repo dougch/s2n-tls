14d0e4ab47be491d5e985cd83a65fb60d9b1569f
Doug Chapman <dougch@amazon.com>
2023-01-26 16:37:02 -0800

    circular references; trying to get clang to work

--- flake.nix
+++ flake.nix
@@ -2,46 +2,78 @@
   description = "A flake for s2n-tls";
 
   inputs =  {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
     flake-utils.url = "github:numtide/flake-utils";
-    s2n-tls.url = "github:dougch/s2n-tls?ref=nix";
+    # TODO: fix circular imports
+    # s2n-tls.url = "github:dougch/s2n-tls/nix";
   };
 
-  outputs = { self, nix, nixpkgs, flake-utils }: 
+  outputs = { self,  nixpkgs, flake-utils }: 
      flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+      llvm = pkgs.pkgsLLVM.llvmPackages_latest;
+      lib = nixpkgs.lib;
    in rec {
       packages.s2n-tls = pkgs.stdenv.mkDerivation {
         src = self;
         name = "s2n-tls";
         inherit system; 
-
-        buildInputs = [ pkgs.cmake
-                        pkgs.ninja
-                        pkgs.openssl_3 ]; # s2n-config has find_dependency LibCrypto
+        # From https://discourse.nixos.org/t/how-to-override-stdenv-for-all-packages-in-mkshell/10368/15
+        nativeBuildInputs = [
+          pkgs.cmake
+          pkgs.pkg-config
+          pkgs.ninja
+          llvm.bintools
+          pkgs.clang-tools_14
+          llvm.lld
+        ];
+        buildInputs = with pkgs; [ 
+                        clang
+                        cmake
+                        ninja
+                        openssl_3 ]; # s2n-config has find_dependency LibCrypto
 
         cmakeFlags = [
             "-DBUILD_SHARED_LIBS=ON"
             "-DUNSAFE_TREAT_WARNINGS_AS_ERRORS=OFF" # disable -Werror
             "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
-            "-DS2N_NO_PQ=1" # TODO: set when system like aarch64
         ];
 
       };
+      packages.default = packages.s2n-tls;
+
+      packages.s2n-tls-openssl3 = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+      });
       packages.s2n-tls-openssl11 = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
         doCheck = true;
         buildInputs = [ pkgs.openssl_1_1 ]; # s2n-config has find_dependency LibCrypto
       });
+      # TODO: crl test fails with libressl-3.6.1
       packages.s2n-tls-libressl = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
         doCheck = true;
         buildInputs = [ pkgs.libressl ]; # s2n-config has find_dependency LibCrypto
       });
-      # TODO: boringssl shared lib no being found by cmake
+      # TODO: shared lib not being found by cmake
       packages.s2n-tls-boringssl = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
         doCheck = true;
         buildInputs = [ pkgs.boringssl ]; # s2n-config has find_dependency LibCrypto
       });
-      defaultPackage = packages.s2n-tls;
+      # TODO: shared lib not being found by cmake
+      packages.s2n-tls-gnutls = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+        buildInputs = [ pkgs.gnutls ]; # s2n-config has find_dependency LibCrypto
+      });
+      # TODO: shared lib not being found by cmake
+      packages.s2n-tls-wolfssl = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+        buildInputs = [ pkgs.wolfssl ]; # s2n-config has find_dependency LibCrypto
+      });
+      # TODO: shared lib not being found by cmake
+      packages.s2n-tls-bearssl = pkgs.s2n-tls.overrideAttrs (finalAttrs: previousAttrs: {
+        doCheck = true;
+        buildInputs = [  ]; # s2n-config has find_dependency LibCrypto
+      });
       
    });
  }
