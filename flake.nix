06cd0b6f1531f131fb63bf1fe16bcd30e7c6d427
Doug Chapman <dougch@amazon.com>
2023-01-27 17:13:58 +0000

    moving llvm stuff to another branch; remove circular import

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   inputs =  {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
     flake-utils.url = "github:numtide/flake-utils";
-    # TODO: fix circular imports
-    # s2n-tls.url = "github:dougch/s2n-tls/nix";
   };
 
   outputs = { self,  nixpkgs, flake-utils }: 
