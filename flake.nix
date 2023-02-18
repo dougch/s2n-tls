65957107ecea3e2a5b400993f84972b697e4f5c6
Doug Chapman <dougch@amazon.com>
2023-02-17 16:06:56 -0800

    Bad formatting test

--- flake.nix
+++ flake.nix
@@ -25,14 +25,14 @@
 
         };
         packages.default = packages.s2n-tls;
-        packages.s2n-tls-openssl3 = packages.s2n-tls.overrideAttrs
+packages.s2n-tls-openssl3 = packages.s2n-tls.overrideAttrs
           (finalAttrs: previousAttrs: { doCheck = true; });
-        packages.s2n-tls-openssl11 = packages.s2n-tls.overrideAttrs
+packages.s2n-tls-openssl11 = packages.s2n-tls.overrideAttrs
           (finalAttrs: previousAttrs: {
             doCheck = true;
             buildInputs = [ pkgs.openssl_1_1 ];
           });
-        packages.s2n-tls-libressl = packages.s2n-tls.overrideAttrs
+packages.s2n-tls-libressl = packages.s2n-tls.overrideAttrs
           (finalAttrs: previousAttrs: {
             doCheck = true;
             buildInputs = [ pkgs.libressl ];
